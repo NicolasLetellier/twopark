f52b8a4b48fe3861927e03e1590a509a1e19816d
NicolasLetellier <mrletelliernicolas@yahoo.fr>
2016-02-25 19:43:55 +0100

    add listener to markers

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -38,7 +38,7 @@
 
 	Map.prototype.fetchParkings = function () {
 		var promise = $.get("/parkings");
-		promise.done(this.retrieveMarkers.bind(this));
+		promise.done(this.dropMarkers.bind(this));
 	};
 
 	Map.prototype.clearMarkers = function () {
@@ -48,29 +48,38 @@
   	this.markers = [];
 	};
 
-	Map.prototype.retrieveMarkers = function (response) {
+	Map.prototype.dropMarkers = function (parkings) {
 		this.clearMarkers();
-		var that = this;
-		for (var i = 0; i < response.length; i++) {
-			if (response[i].available) {
-				var position = {lat: parseFloat(response[i].lat), lng: parseFloat(response[i].long)};
-				var title = response[i].title;
-				that.createMarkersWithTimeout(position, title, i*100);
+		// add all parkings objects to an array
+		for (var i = 0; i < parkings.length; i++) {
+			if (parkings[i].available) {
+				var position = {lat: parseFloat(parkings[i].lat), lng: parseFloat(parkings[i].long)};
+				var title = parkings[i].title;
+				var parkingId = parkings[i].id;
+				this.createMarkersWithTimeout(position, title, parkingId, i*80);
 			}
 		};
 	};
 
-	Map.prototype.createMarkersWithTimeout = function (position, title, timeout) {
+	Map.prototype.createMarkersWithTimeout = function (position, title, parkingId, timeout) {
 		var that = this;
 		window.setTimeout(function(){
-			that.markers.push(new google.maps.Marker({
+			var marker = new google.maps.Marker({
 				position: position,
 				map: that.googleMap,
 				title: title,
+				parkingId: parkingId,
 				animation: google.maps.Animation.DROP
-			}));
+			});
+			marker.addListener("click", that.clickMarker);
+			that.markers.push(marker);
 		}, timeout);
 	};
+
+	Map.prototype.clickMarker = function () {
+		// .html >> sobresecribe ant
+		console.log("bueno");
+	}
 
 	Map.prototype.displayMap = function () {
 		var stylesArrayPaledawn = [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},
@@ -84,30 +93,12 @@
 			{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},
 			{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]},
 			{"featureType":"transit.line","stylers":[{"visibility":"off"}]}];
-		
-		var stylesArrayLightdream = [{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}];
-		var stylesArraySubtlegrayscale = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-96}]}];
-		
-		var stylesArrayBlueessence = [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},
-			{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},
-			{"featureType":"poi.business","elementType":"geometry","stylers":[{"visibility":"off"}],"elementType":"labels","stylers":[{"visibility":"off"}]},
-			{"featureType":"road","elementType":"geometry","stylers":[{"lightness":50},{"visibility":"on"}]},
-			{"featureType":"road","elementType":"labels.icon","stylers":[{"saturation":-50},{"lightness":30},{"visibility":"on"}]},
-			{"featureType":"road.local","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
-			{"featureType":"road","elementType":"labels","stylers":[{"visibility":"on"}]},
-			{"featureType":"transit.line","stylers":[{"visibility":"off"}]}, 
-			{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}];
-
 		this.googleMap = new google.maps.Map(document.getElementById("map"), {
 			zoom: 14,
 			center: new google.maps.LatLng(this.coordinates.latitude, this.coordinates.longitud),
 		});
-
 		this.googleMap.setOptions({styles: stylesArrayPaledawn});
-
 		this.fetchParkings();
-
 	};
-
 
 })();
